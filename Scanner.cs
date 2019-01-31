28c11bc7c68b36a51462c4b6b9a2e0ff37a1427d
Kenneth Cochran <kenneth.cochran101@gmail.com>
2019-01-31 07:09:22 -0600

    partial implementation

--- Scanner.cs
+++ Scanner.cs
@@ -1,129 +1,101 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Text;
 using System.Linq;
 
 namespace SqlCompiler.StringScanner
 {
-    public class Scanner : System.IO.StringReader
+    public class Scanner : System.IO.StreamReader
     {
-        private StringBuilder buffer = new StringBuilder();
-
-        public Scanner(string content) : base(content) { }
+        public Scanner(string content) :
+        base(new MemoryStream(Encoding.UTF8.GetBytes(content)))
+        { }
 
         // This method is stateful, resetting every other time it is called.
         // The first time, it reads until a delimiter is matched.
         // The second time, it reads until the end of the delimiter, and 
         // resets it's state (the this.buffer).
-        public string ReadNextWordOrDelim(IEnumerable<Regex> delimiters)
+        public string ReadNextWordOrDelim(DelimiterCollection delimiters)
         {
+            StringBuilder buffer = new StringBuilder();
+            long oldPosition = BaseStream.Position;
             string ret;
 
+            // Read until we find a delimiter match.
             do
             {
-                // If we're in the middle of matching a closing delimiter
-                if (delimiters.Any(r => r.IsMatch(this.buffer.ToString())))
-                {
-                    ret = ReadNextDelim(delimiters);
-                    this.buffer.Clear();
-                }
-                else
-                {
-                    ret = ReadNextWord(delimiters);
-                }
-            } while (String.IsNullOrEmpty(ret) && Peek() != -1);
+                buffer.Append(Read());
+            } while (delimiters.IsMatch(buffer.ToString()));
+
+            Match match = delimiters.LongestMatch(buffer.ToString());
+
+            // We are at EOL.
+            if (match == null) return buffer.ToString();
+
+
+            Seek(oldPosition + match.Index);
+
+            // If we're reading a delim;
+            if (match.Index == 0)
+            {
+                ret = ReadDelim(delimiters);
+            }
+            else
+            {
+                ret = buffer.ToString(0, match.Index);
+            }
+
             return ret;
+
         }
 
-        /// <summary>
-        /// Reads the next word.
-        /// </summary>
-        /// <returns>The next word.</returns>
-        private string ReadNextWord(IEnumerable<Regex> delimiters)
+        private void Seek(long position)
         {
-            int next;
+            BaseStream.Seek(position, SeekOrigin.Begin);
+            DiscardBufferedData();
+        }
+
+        private string ReadDelim(DelimiterCollection delimiters)
+        {
+            StringBuilder buffer = new StringBuilder();
+            int matchLength;
             do
             {
-                next = Read();
-                if (next == -1)
-                {
-                    return this.buffer.ToString();
-                }
-                this.buffer.Append((char)next);
-            } while (!GetDelimLocation(this.buffer.ToString(), delimiters).HasValue);
-
-            var (start, length) = GetDelimLocation(this.buffer.ToString(), delimiters).Value;
-            return this.buffer.ToString().Substring(0, start);
-        }
-
-        /// <summary>
-        /// Reads the next delimiter. Assumes the buffer has previous content.
-        /// </summary>
-        /// <returns>The next delimiter.</returns>
-        private string ReadNextDelim(IEnumerable<Regex> delimiters)
-        {
-            while (KeepReadingDelim(delimiters))
-            {
-                this.buffer.Append((char)Read());
-            }
-            return JustTheDelim(this.buffer.ToString(), delimiters);
-        }
-
-        /// <summary>
-        /// Should we keep adding to the delimiter?
-        /// </summary>
-        /// <remarks>
-        /// Assumes that GetDelimLocation will return non-null data at the current state
-        /// </remarks>
-        /// <returns><c>true</c>, if the next character can be included in the delimiter, <c>false</c> otherwise.</returns>
-        private bool KeepReadingDelim(IEnumerable<Regex> delimiters)
-        {
-            int next = Peek();
-            if (next == -1)
-            {
-                return false;
-            }
-            var (_, oldLength) = GetDelimLocation(this.buffer.ToString(), delimiters).Value;
-            var (_, newLength) = GetDelimLocation(this.buffer.ToString() + (char)next, delimiters).Value;
-
-            return newLength > oldLength;
-        }
-
-        /// <summary>
-        /// The start and end of the match for the longest _delimiter
-        /// </summary>
-        /// <returns>The beginning and end of the longest delimiter</returns>
-        /// <param name="content">The string to check for a delimiter.</param>
-        private (int start, int length)? GetDelimLocation(string content, IEnumerable<Regex> delimiters)
-        {
-            Match maxMatch = delimiters
-                .SelectMany(r => (IEnumerable<Match>) r.Matches(content))
-                .OrderByDescending(m => m.Length)
-                .FirstOrDefault();
-           
-            if (maxMatch == null)
-            {
-                return null;
-            }
-            return (maxMatch.Index, maxMatch.Length);
-        }
-
-        /// <summary>
-        /// Longest match for any _delimiter within content. "" if none.
-        /// </summary>
-        /// <returns>The text of the longest delimiter</returns>
-        /// <param name="content">The string to pull the delimiter from.</param>
-        private string JustTheDelim(string content, IEnumerable<Regex> delimiters)
-        {
-            string ret = "";
-            var startEnd = GetDelimLocation(content, delimiters);
-            if (startEnd.HasValue)
-            {
-                var (start, length) = startEnd.Value;
-                ret = content.Substring(start, length);
-            }
-            return ret;
+                buffer.Append
+                matchLength = delimiters.LongestMatch(buffer.ToString()).Length;
+            } while (matchLength == buffer.Length);
+            return buffer.ToString(0, matchLength);
         }
     }
+
+    abstract class DelimiterCollection : IEnumerable<Regex>
+    {
+        private List<Regex> delimiters = new List<Regex>();
+        public List<Regex> Delimiters { get; set; }
+
+        public bool IsMatch(string content)
+        {
+            return delimiters.Any(d => d.IsMatch(content));
+        }
+        public Match LongestMatch(string content)
+        {
+            return delimiters.Select(d => d.Match(content))
+                             .FirstOrDefault(m => m.Success);
+        }
+
+        #region interface methods
+        public IEnumerator<Regex> GetEnumerator()
+        {
+            return delimiters.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        #endregion
+    }
+    
 }
