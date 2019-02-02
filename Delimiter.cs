fe24f97684ca54aa1ad8e1c916ec8c5f841e04fa
Kenneth Cochran <kenneth.cochran101@gmail.com>
2019-02-01 20:41:25 -0600

    Scanner has been refactored

---
+++ Delimiter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace SqlCompiler.StringScanner
+{
+    public interface IDelimiter
+    {
+        Match Match(string toMatch);
+        bool IsMatch(string toMatch);
+    }
+
+    public abstract class DelimiterCollection<T> : ICollection<T>, IDelimiter
+        where T : IDelimiter
+    {
+        public ICollection<T> Delimiters { get; set; }
+
+        public DelimiterCollection() : this(new List<T>()) { }
+        public DelimiterCollection(ICollection<T> delimiters)
+        {
+            Delimiters = delimiters;
+        }
+
+        public bool IsMatch(string content)
+        {
+            return Delimiters.Any(d => d.IsMatch(content));
+        }
+        /// <summary>
+        /// Returns the longest match of a given string.
+        /// </summary>
+        /// <returns>The longest match.</returns>
+        /// <param name="content">The string to match.</param>
+        public Match Match(string content)
+        {
+            return Delimiters.Select(d => d.Match(content))
+                             .OrderByDescending(m => m.Length)
+                             .FirstOrDefault();
+        }
+
+        #region proxied interface methods
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Delimiters.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count => Delimiters.Count();
+
+        public abstract bool IsReadOnly { get; }
+
+        public void Add(T item)
+        {
+            Delimiters.Add(item);
+        }
+
+        public void Clear()
+        {
+            Delimiters.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return Delimiters.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Delimiters.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return Delimiters.Remove(item);
+        }
+        #endregion
+    }
+
+}
--- Scanner.cs
+++ Scanner.cs
@@ -1,55 +1,66 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Text;
-using System.Linq;
 
 namespace SqlCompiler.StringScanner
 {
     public class Scanner : System.IO.StreamReader
     {
         public Scanner(string content) :
-        base(new MemoryStream(Encoding.UTF8.GetBytes(content)))
-        { }
+        base(new MemoryStream(Encoding.UTF8.GetBytes(content))) { }
 
         // This method is stateful, resetting every other time it is called.
         // The first time, it reads until a delimiter is matched.
         // The second time, it reads until the end of the delimiter, and 
         // resets it's state (the this.buffer).
-        public string ReadNextWordOrDelim(DelimiterCollection delimiters)
+        private string Read(Func<string, int> substringTo)
         {
             StringBuilder buffer = new StringBuilder();
             long oldPosition = BaseStream.Position;
-            string ret;
+            int readTo;
 
-            // Read until we find a delimiter match.
             do
             {
-                buffer.Append(Read());
-            } while (delimiters.IsMatch(buffer.ToString()));
+                int character = Read();
+                if (character == -1)
+                {
+                    return buffer.Length > 0 ? buffer.ToString() : null;
+                }
+                buffer.Append((char)character);
+            } while ((readTo = substringTo(buffer.ToString())) == -1);
 
-            Match match = delimiters.LongestMatch(buffer.ToString());
+            Seek(oldPosition + readTo);
+            return buffer.ToString(0, readTo);
+        }
 
-            // We are at EOL.
-            if (match == null) return buffer.ToString();
-
-
-            Seek(oldPosition + match.Index);
-
-            // If we're reading a delim;
-            if (match.Index == 0)
+        public string Read(IDelimiter delimiter)
+        {
+            // Read until we have a match.
+            string word = Read(s =>
             {
-                ret = ReadDelim(delimiters);
-            }
-            else
-            {
-                ret = buffer.ToString(0, match.Index);
-            }
+                var m = delimiter.Match(s);
+                if (!m.Success) return -1;
+                return m.Index;
+            });
 
+            // Read until we find a delimiter match.
+            if (String.IsNullOrEmpty(word))
+            {
+                word = Read(s =>
+                {
+                    int matchLength = delimiter.Match(s).Length;
+                    return matchLength == 0 || matchLength == s.Length ? -1 : matchLength;
+                });
+            }
+            return word;
+        }
+
+        public string Peek(IDelimiter delimiter)
+        {
+            long oldPosition = BaseStream.Position;
+            string ret = Read(delimiter);
+            Seek(oldPosition);
             return ret;
-
         }
 
         private void Seek(long position)
@@ -57,45 +68,5 @@ namespace SqlCompiler.StringScanner
             BaseStream.Seek(position, SeekOrigin.Begin);
             DiscardBufferedData();
         }
-
-        private string ReadDelim(DelimiterCollection delimiters)
-        {
-            StringBuilder buffer = new StringBuilder();
-            int matchLength;
-            do
-            {
-                buffer.Append
-                matchLength = delimiters.LongestMatch(buffer.ToString()).Length;
-            } while (matchLength == buffer.Length);
-            return buffer.ToString(0, matchLength);
-        }
-    }
-
-    abstract class DelimiterCollection : IEnumerable<Regex>
-    {
-        private List<Regex> delimiters = new List<Regex>();
-        public List<Regex> Delimiters { get; set; }
-
-        public bool IsMatch(string content)
-        {
-            return delimiters.Any(d => d.IsMatch(content));
-        }
-        public Match LongestMatch(string content)
-        {
-            return delimiters.Select(d => d.Match(content))
-                             .FirstOrDefault(m => m.Success);
-        }
-
-        #region interface methods
-        public IEnumerator<Regex> GetEnumerator()
-        {
-            return delimiters.GetEnumerator();
-        }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
-        }
-        #endregion
-    }
-    
+    }    
 }
